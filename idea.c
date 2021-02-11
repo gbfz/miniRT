fdccb331816cde8586048ec06a6141325c2b759b
Gosha <gbfiglin@gmail.com>
2021-02-11 23:37:35 +0300

    ideas about o_c and t[2]

--- idea.c
+++ idea.c
@@ -10,7 +10,7 @@ static int		intersection(t_cylinder *cyl, float ray[3], float **p, float **n)
 	float eq[3];
 	float o_c[3];
 	float disc;
-	float t[3];
+	float t[2];
 
     get_o_c(cyl, o_c);
 	eq[0] = dot(ray, ray) - pow(dot(ray, cyl->vector), 2);
@@ -19,15 +19,14 @@ static int		intersection(t_cylinder *cyl, float ray[3], float **p, float **n)
 	disc = eq[1] * eq[1] - 4 * eq[0] * eq[2];
 	if (disc < 0)
 		return (0);
-	t[1] = (-eq[1] - sqrt(disc)) / (2 * eq[0]);
-	t[2] = (-eq[1] + sqrt(disc)) / (2 * eq[0]);
-	if (check_borders(cyl, t[1], ray, o_c) == 1)
-		t[0] = t[1];
-	else if (check_borders(cyl, t[2], ray, o_c) == 1)
-		t[0] = t[2];
+	t[0] = (-eq[1] - sqrt(disc)) / (2 * eq[0]);
+	t[1] = (-eq[1] + sqrt(disc)) / (2 * eq[0]);
+	if (check_borders(cyl, t[0], ray, o_c) == 1)
+        *p = get_point(ray, t[0]);
+	else if (check_borders(cyl, t[1], ray, o_c) == 1)
+        *p = get_point(ray, t[1]);
 	else
 		return (0);
-	*p = get_point(ray, t[0]);
 	*n = get_normal(*p, cyl->coords);
 	return (1);
 }
